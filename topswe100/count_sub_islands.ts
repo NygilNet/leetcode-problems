f54b6cc1313c8dd90b3ca63ecf3fa2eb38453528
nygilnet <nygilnet@gmail.com>
2024-02-20 12:09:12 -0600

    2024/02/20 topswe100

--- topswe100/count_sub_islands.ts
+++ topswe100/count_sub_islands.ts
@@ -6,7 +6,7 @@ link to problem:
 
 function countSubIslands(grid1: number[][], grid2: number[][]): number {
     let maxArea = 0;
-    const DIRECTIONS = [[0, 1], [0, -1], [1, 0], [-1, 0]], numOfRows = grid1.length, numOfCols = grid1[0].length, visited: Set<string> = new Set();
+    const DIRECTIONS = [[0, 1], [0, -1], [1, 0], [-1, 0]], numOfRows = grid1.length, numOfCols = grid1[0].length, visited = Array.from({ length: numOfCols }, () => new Array(numOfRows).fill(0));
 
     function _isInBounds(r: number, c: number): boolean {
         return (0 <= r && r < numOfRows) && (0 <= c && c < numOfCols); 
@@ -19,14 +19,14 @@ function countSubIslands(grid1: number[][], grid2: number[][]): number {
     
     function _mapIsland(row: number, col: number) {
         const stack = [col, row];
-        visited.add(`${row},${col}`);
+        visited[row][col] = "X";
         while (stack.length) {
             const r = stack.pop()!;
             const c = stack.pop()!;
             for (const [dr, dc] of DIRECTIONS) {
                 const newRow = r + dr, newCol = c + dc;
-                if (_isInBounds(newRow, newCol) && grid1[newRow][newCol] === 1 && !visited.has(`${row},${col}`)) {
-                    visited.add(`${newRow},${newCol}`);
+                if (_isInBounds(newRow, newCol) && grid1[newRow][newCol] === 1 && !visited[newRow][newCol]) {
+                    visited[newRow][newCol] = "X";
                     stack.push(newCol, newRow);
                 } 
             }
@@ -36,7 +36,7 @@ function countSubIslands(grid1: number[][], grid2: number[][]): number {
 
     for (let row = 0; row < numOfRows; row++) {
         for (let col = 0; col < numOfCols; col++) {
-            if (grid1[row][col] === 1 && !visited.has(`${row},${col}`)) _mapIsland(row, col);    
+            if (grid1[row][col] === 1 && !visited[row][col]) _mapIsland(row, col);    
         }
     }
 
