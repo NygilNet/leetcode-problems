4b6ebad37c866e2c61ab5be75a308caa73bd3b84
nygilnet <nygilnet@gmail.com>
2024-03-28 16:25:27 -0500

    2024/03/28 open dsa session with alex longest univalue path

---
+++ longest_univalue_path.ts
@@ -0,0 +1,44 @@
+/*
+
+link to leetcode: https://leetcode.com/problems/longest-univalue-path/description/
+
+Given the root of a binary tree, return the length of the longest path, where each node in the path has the same value. This path may or may not pass through the root.
+
+The length of the path between two nodes is represented by the number of edges between them.
+
+Input: root of TreeNode
+Output: number of edges of longest path
+
+
+dfs to traverse entire tree
+
+    base case: node is null, return 0
+    check if parent has the same value of child(ren)
+    return max(left, right) + 1
+    if child does not equal the current node, do not take its return value
+
+
+*/
+
+function longestUnivaluePath(root: TreeNode | null): number {
+    let longest = 0;
+
+    function _dfs(node: TreeNode | null): number {
+        if (!node) {
+            return 0;
+        }
+
+        const { val, left, right } = node;
+
+        let leftPath: number = left?.val === val ? _dfs(node.left) : 0;
+        let rightPath: number = right?.val === val ? _dfs(node.right): 0;
+
+        const pathLen = leftPath + rightPath + 1;
+        longest = Math.max(longest, pathLen);
+
+        return pathLen;
+    }
+
+    _dfs(root);
+    return longest;
+}
