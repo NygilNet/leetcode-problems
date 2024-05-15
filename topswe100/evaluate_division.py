c41c8238a6918190d51f7a943180382afd6b8116
nygilnet <nygilnet@gmail.com>
2024-05-14 22:03:53 -0500

    2024/05/14 15 minute progress on evaluate divison leetcode problem -> whiteboarding

---
+++ topswe100/evaluate_division.py
@@ -0,0 +1,22 @@
+"""
+link to problem: https://leetcode.com/problems/evaluate-division/description/
+
+You are given an array of variable pairs equations and an array of real numbers values, where equations[i] = [Ai, Bi] and values[i] represent the equation Ai / Bi = values[i]. Each Ai or Bi is a string that represents a single variable.
+
+You are also given some queries, where queries[j] = [Cj, Dj] represents the jth query where you must find the answer for Cj / Dj = ?.
+
+Return the answers to all queries. If a single answer cannot be determined, return -1.0.
+
+Note: The input is always valid. You may assume that evaluating the queries will not result in division by zero and that there is no contradiction.
+
+Note: The variables that do not occur in the list of equations are undefined, so the answer cannot be determined for them.
+
+ 
+INPUT: equations (array of arrays with two strings), values (array of floats), queries (array of arrays with two strings)
+OUTPUT: array of floats (the final solution of each query)
+
+problem 1 - solve for variables:
+
+    i. initialize a map to store the value of each variable we come across in the equations array
+
+"""
