07bcf525e9428acb5a90e5e2194fe08d0c7cba9f
nygilnet <nygilnet@gmail.com>
2024-06-01 16:18:05 -0500

    2024/06/01 longest substring without repeating characters O(n) solution

--- longest_substring_without_repeating_characters.py
+++ longest_substring_without_repeating_characters.py
@@ -13,20 +13,41 @@ Output: number
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        left = 0
-        longest = 1
+        if not s:
+            return 0
+        
+        letters_encountered = {}
+        start = 0
+        length = 0
         N = len(s)
 
-        while left < N:
-            right = left + 1
-            letters_encountered = set()
-            letters_encountered.add(s[left])
+        for end in range(N):
+            char = s[end]
 
-            while right < N and s[right] not in letters_encountered:
-                letters_encountered.add(s[right])
-                right += 1
+            if char in letters_encountered and start <= letters_encountered[char]:
+                length = max(length, end - start)
+                start = letters_encountered[char] + 1
 
-            longest = max(longest, right - left)
-            left += 1
+            letters_encountered[char] = end
 
-        return longest
+        length = max(length, end - start + 1)
+        return length
+
+
+        # left = 0
+        # longest = 1
+        # N = len(s)
+
+        # while left < N:
+        #     right = left + 1
+        #     letters_encountered = set()
+        #     letters_encountered.add(s[left])
+
+        #     while right < N and s[right] not in letters_encountered:
+        #         letters_encountered.add(s[right])
+        #         right += 1
+
+        #     longest = max(longest, right - left)
+        #     left += 1
+
+        # return longest
