743e85c0c89bf3bd1f5fa56003367d106d0e2cfa
nygilnet <nygilnet@gmail.com>
2024-06-22 19:54:41 -0500

    2024/06/22 more efficent solution for shortest substring containing all characters

--- dailycodingproblem/problem_103.py
+++ dailycodingproblem/problem_103.py
@@ -14,30 +14,49 @@ output: shortest substring that has all characters or None
 """
 
 class Solution:
-    def shortestSubstringContainingCharacters(self, s: str, chars: set[str]) -> (str | None):
+    def shortestSubstringContainingCharacters(s: str, chars: set[str]):
         N = len(s)
-        self.shortest_substring = "x" * (N + 1)
-        print(self.shortest_substring)
-        def _backtracking(idx: int, remainingChars: set[str], substring: str = ""):
-            if len(remainingChars) == 0:
-                if len(substring) < len(self.shortest_substring):
-                    self.shortest_substring  = substring
-                return
-            if idx == N:
-                return
-            
-            char = s[idx]
-            substring += char
-
-            if char in remainingChars:
-                remainingChars.remove(char)
-
-            _backtracking(idx + 1, remainingChars, substring)
+        shortest_substring = ""
+        indices = {}
         
         for i in range(N):
-            _backtracking(i, chars)
+            char = s[i]
 
-        return self.shortest_substring if self.shortest_substring is not "x" * (N + 1) else None
+            if char in chars:
+                indices[char] = i
+
+            if len(indices) == len(chars):
+                values = indices.values()
+                earliest, latest = min(values), max(values)
+                new_substring = s[earliest : latest + 1]
+                if not shortest_substring or len(new_substring) < len(shortest_substring):
+                    shortest_substring = new_substring
+
+        return shortest_substring if len(shortest_substring) > 0 else None
+        
+        # N = len(s)
+        # self.shortest_substring = "x" * (N + 1)
+        # print(self.shortest_substring)
+        # def _backtracking(idx: int, remainingChars: set[str], substring: str = ""):
+        #     if len(remainingChars) == 0:
+        #         if len(substring) < len(self.shortest_substring):
+        #             self.shortest_substring  = substring
+        #         return
+        #     if idx == N:
+        #         return
+            
+        #     char = s[idx]
+        #     substring += char
+
+        #     if char in remainingChars:
+        #         remainingChars.remove(char)
+
+        #     _backtracking(idx + 1, remainingChars, substring)
+        
+        # for i in range(N):
+        #     _backtracking(i, chars.copy())
+
+        # return self.shortest_substring if self.shortest_substring is not "x" * (N + 1) else None
     
 
 print(Solution.shortestSubstringContainingCharacters("figehaeci", set(['a', 'e', 'i'])))
