30e317c8c188c1a03ab7957d7ba26f114f96e113
nygilnet <nygilnet@gmail.com>
2024-08-01 16:42:41 -0500

    2024/08/01 open dsa with alex group solution

--- flatten_binary_tree_to_linked_list.py
+++ flatten_binary_tree_to_linked_list.py
@@ -14,21 +14,43 @@ class TreeNode:
         self.left = left
         self.right = right
 
-from collections import deque
+# from collections import deque
 class Solution:
     def flatten(self, root: TreeNode | None):
-        def _preorder(r: TreeNode | None) -> list[TreeNode]:
-            if not r:
-                return []
-            return [r] + _preorder(r.left) + _preorder(r.right)
+        def _dfs(node):
+            if not node:
+                return None
+            
+            left_tail = _dfs(node.left)
+            right_tail = _dfs(node.right)
+
+            if node.left:
+                tmp = node.right
+                node.right = node.left
+                left_tail.right = tmp
+                node.left = None
+
+            if right_tail:
+                return right_tail
+            elif left_tail:
+                return left_tail
+            else:
+                return node
         
-        stack = deque(_preorder(root))
-        while stack:
-            curr = stack.popleft()
-            n = stack[0] if stack else None
+        _dfs(root)
 
-            curr.left = None
-            curr.right = n
+        # def _preorder(r: TreeNode | None) -> list[TreeNode]:
+        #     if not r:
+        #         return []
+        #     return [r] + _preorder(r.left) + _preorder(r.right)
+        
+        # queue = deque(_preorder(root))
+        # while queue:
+        #     curr = queue.popleft()
+        #     nxt = queue[0] if queue else None
 
-        return root
+        #     curr.left = None
+        #     curr.right = nxt
+
+        # return
             
